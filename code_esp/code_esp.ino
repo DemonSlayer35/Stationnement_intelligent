0e7398ad008b7cdabc318078ec5cde2a116fb8a6
Derio88 <etiennedube@gmail.com>
2023-05-02 11:11:58 -0400

    Ajout du code de la barriere

--- code_esp/code_esp.ino
+++ code_esp/code_esp.ino
@@ -6,7 +6,7 @@
 #include "SPIFFS.h"
 #include <PubSubClient.h>
 
-const char* ssid     = "A113";       /*Remplacer par le SSID de votre réseau*/
+const char* ssid     = "tge_sansfil_283";       /*Remplacer par le SSID de votre réseau*/
 const char* password = "sherbrooke"; /*Remplacer par le mot de passe de votre réseau*/
 
 // MQTT Broker
@@ -14,13 +14,28 @@ const char *mqtt_broker = "192.168.0.101"; /*Remplacer par l'adresse IP du broke
 const char *topic = "parking/A";           /*Remplacer par le topic MQTT*/
 const int mqtt_port = 1883;
 
+/********Barriere*******/
+#define RED_LIGHT 23
+#define GREEN_LIGHT 22
+#define GATE_OPEN 18
+#define GATE_CLOSE 17
+#define AVANT 5
+#define APRES 16
+#define OUVERTURE 15
+#define FERMETURE 4
+
+int c_open = 0;
+int c_close = 0;
+int c_avant = 0;
+int c_apres = 0;
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 DNSServer dnsServer;
 AsyncWebServer server(80);
 
-String option1 = ""; // string pour stocker la liste JSON des données du parking
+String liste = ""; // string pour stocker la liste JSON des données du parking
 
 // Fonction pour lister les fichiers sur le système de fichiers SPIFFS.
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
@@ -72,7 +87,7 @@ public:
 // Fonction pour mettre à jour la page Web avec les données du parking
 void update(){
   server.on("/content", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/json", option1);
+    request->send(200, "text/json", liste);
   });
 }
 
@@ -91,7 +106,7 @@ void setupServer(){
    
   // Route qui répond à une requête HTTP GET sur le contenu du parking (format JSON)
   server.on("/content", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/json", option1);
+    request->send(200, "text/json", liste);
   });
 }
 
@@ -136,6 +151,7 @@ void setup(){
   Serial.println(WiFi.localIP()); // Affiche l'adresse IP de l'ESP
 
   // Configuration de la connexion au broker MQTT
+  /*
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
   while (!client.connected()) {
@@ -145,6 +161,7 @@ void setup(){
       Serial.printf("The client %s connects to the mqtt broker\n", client_id.c_str());
      
       // Tentative de connexion au broker MQTT
+      
       if (client.connect(client_id.c_str())) {
           Serial.println("MQTT connected");
           client.setBufferSize(1024);  // change la taille du buffer à 1024 parce que le message JSON est volumineux
@@ -154,7 +171,21 @@ void setup(){
           Serial.print(client.state());
           delay(2000);
       }
-  }
+  }*/
+
+
+  //***************Code pour la barrière*******************//
+
+  pinMode(4,OUTPUT);
+  pinMode(15,OUTPUT);
+  pinMode(21, INPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23,OUTPUT);
+  pinMode(5, INPUT);
+  pinMode(16, INPUT);
+  pinMode(17, INPUT);
+  pinMode(18, INPUT);
+
 }
 
 // Callback appelé lorsque le client MQTT reçoit un message
@@ -164,23 +195,82 @@ void callback(char *topic, byte *payload, unsigned int length) {
   Serial.print("Message:");
    
   // On initialise l'option à une chaîne vide
-  option1 = "";
+  liste = "";
   
   // Conversion des données reçues en chaîne de caractères
   for (int i = 0; i < length; i++) {
-      option1 += (char)payload[i];
+      liste += (char)payload[i];
   }
   
-  Serial.println(option1);
+  Serial.println(liste);
   Serial.println();
   Serial.println("-----------------------");
+
+  // On compte le nombre de fois que le mot "libre" apparaît dans le message reçu
+  int count = 0;
+  int pos = liste.indexOf("libre");
+  while (pos >= 0) {
+    count++;
+    pos = liste.indexOf("libre", pos + 1);
+  }
+  Serial.print("Le nombre de places libres est ");
+  Serial.println(count);
   
   // On appelle la fonction update() pour mettre à jour la page Web avec les nouvelles données du parking
   update();
 }
 
+void fermer()
+{
+  digitalWrite((int)OUVERTURE,LOW);
+    digitalWrite((int)FERMETURE,HIGH);
+    while(c_close == 1)
+    {
+      c_open = digitalRead(GATE_OPEN);
+      c_close = digitalRead(GATE_CLOSE);
+    }
+    digitalWrite(OUVERTURE,LOW);
+    digitalWrite(FERMETURE,LOW);
+}
 
+void ouvrir()
+{
+  digitalWrite((int)FERMETURE,LOW);
+    digitalWrite((int)OUVERTURE,HIGH);
+    while(c_open == 1)
+    {
+      c_open = digitalRead(GATE_OPEN);
+      c_close = digitalRead(GATE_CLOSE);
+    }
+    digitalWrite(FERMETURE,LOW);
+    digitalWrite(OUVERTURE,LOW);
+}
 void loop(){
-  dnsServer.processNextRequest(); // On gère les requêtes DNS
-  client.loop(); // On maintient la connexion MQTT active
+  //dnsServer.processNextRequest(); // On gère les requêtes DNS
+  //client.loop(); // On maintient la connexion MQTT active
+
+  //***************Code pour la barrière*******************//
+  c_open = digitalRead(GATE_OPEN);
+  c_close = digitalRead(GATE_CLOSE);
+  c_avant = digitalRead(AVANT);
+  c_apres = digitalRead(APRES);
+  //Serial.printf("Close : %d  Open : %d  apres :%d  avant : %d\n\r",c_close,c_open,c_apres,c_avant);
+  if((c_close == 0 && c_open == 1 && c_avant == 0)|(c_close == 0 && c_open == 1 && digitalRead(21) == 0) )// Si la barriere est fermée
+  {
+    ouvrir();
+  }
+  if((c_close == 1 && c_open == 0 && c_apres == 0)|(c_close == 1 && c_open == 0 && digitalRead(21) == 0))// Si la barriere est ouverte
+  {
+    fermer();
+  }
+  if (c_open == 0)
+  {
+    digitalWrite(RED_LIGHT,LOW);
+    digitalWrite(GREEN_LIGHT,HIGH);
+  }else
+  {
+    digitalWrite(RED_LIGHT,HIGH);
+    digitalWrite(GREEN_LIGHT,LOW);
+  }
+  
 }
